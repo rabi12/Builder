3b1b0b13489c9ead703f534778d03ad9da76a8c1
rabi12 <rabi.aranda@gmail.com>
2020-01-14 01:44:45 -0600

    Patrong Builder
    Practica

---
+++ Patron Builder/Patron Builder/Director.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patron_Builder
+{
+    public class Director
+    {
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+
+        // The Director can construct several product variations using the same
+        // building steps.
+        public void buildMinimalViableProduct()
+        {
+            this._builder.BuildPartA();
+        }
+
+        public void buildFullFeaturedProduct()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+    }
+}
---
+++ Patron Builder/Patron Builder/IBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patron_Builder
+{
+    public interface IBuilder
+    {
+        void BuildPartA();
+
+        void BuildPartB();
+
+        void BuildPartC();
+    }
+}
